ca9de6f13715d63c063c425ccf68dc5f59a75e28
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-01-04 13:40:52 -0300

    random number

--- scripts.js
+++ scripts.js
@@ -1,6 +1,21 @@
 const raffle = document.querySelector(".raffle");
+const quantity = document.getElementById("quantity");
+const start = document.getElementById("start");
+const end = document.getElementById("end");
 
 raffle.addEventListener("click", (event) => {
   event.preventDefault();
-  alert("You have been entered into the raffle!");
+
+  const quantityValue = Number(quantity.value);
+  const startValue = Number(start.value);
+
+  const endValue = Number(end.value);
+
+  const randomNumber = Math.floor(Math.random() * (endValue - startValue + 1)) + startValue;
+  console.log(randomNumber);
 });
+
+function computerSelectionOfNumber() {
+
+ 
+}
