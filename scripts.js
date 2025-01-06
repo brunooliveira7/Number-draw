f025873f044f7505af3a689fbd126f494b57632a
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-01-06 10:29:48 -0300

    adjustment

--- scripts.js
+++ scripts.js
@@ -7,52 +7,52 @@ const form = document.querySelector("form");
 const result = document.querySelector(".result");
 
 raffle.addEventListener("click", (event) => {
- event.preventDefault();
+  event.preventDefault();
 
- const quantityValue = Number(quantity.value);
- const startValue = Number(start.value);
- const endValue = Number(end.value);
+  const quantityValue = Number(quantity.value);
+  const startValue = Number(start.value);
+  const endValue = Number(end.value);
 
- const drawnNumber =
- Math.floor(Math.random() * (endValue - startValue + 1)) + startValue;
+  const drawnNumber =
+    Math.floor(Math.random() * (endValue - startValue + 1)) + startValue;
 
- const title = document.createElement("h4");
- title.textContent = "Draw result";
- const subtitle = document.createElement("p");
- subtitle.textContent = "1st result";
- const resultContainer = document.createElement("span");
- resultContainer.textContent = drawnNumber;
+  const title = document.createElement("h4");
+  title.textContent = "Resultado do sorteio";
+  const subtitle = document.createElement("p");
+  subtitle.textContent = "1º resultado";
+  const resultContainer = document.createElement("span");
+  resultContainer.textContent = drawnNumber;
 
- const buttonAgain = document.createElement("button");
- const img = document.createElement("img");
- img.setAttribute("src", "./assets/again.svg");
- buttonAgain.classList.add("again");
- buttonAgain.textContent = "Draw again";
- buttonAgain.appendChild(img);
+  const buttonAgain = document.createElement("button");
+  const img = document.createElement("img");
+  img.setAttribute("src", "./assets/again.svg");
+  buttonAgain.classList.add("again");
+  buttonAgain.textContent = "Sortear novamente";
+  buttonAgain.appendChild(img);
 
- result.appendChild(title);
- result.appendChild(subtitle);
- result.appendChild(resultContainer);
- result.appendChild(buttonAgain);
+  result.appendChild(title);
+  result.appendChild(subtitle);
+  result.appendChild(resultContainer);
+  result.appendChild(buttonAgain);
 
- toggleScreen();
- buttonAgain.addEventListener("click", () => {
- buttonAgain.classList.add("hidden");
- result.classList.add("hidden");
- form.classList.remove("hidden");
- });
- clearInput();
+  toggleScreen();
+  buttonAgain.addEventListener("click", () => {
+    buttonAgain.classList.add("hidden");
+    result.classList.add("hidden");
+    form.classList.remove("hidden");
+  });
+  clearInput();
 });
 
 function randomNumber() {}
 
 function toggleScreen() {
- form.classList.toggle("hidden");
- result.classList.toggle("hidden");
+  form.classList.toggle("hidden");
+  result.classList.toggle("hidden");
 }
 
 function clearInput() {
- quantity.value = "";
- start.value = "";
- end.value = "";
+  quantity.value = "";
+  start.value = "";
+  end.value = "";
 }
