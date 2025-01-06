38344df09061e64f50db8344738673d27137118d
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-01-06 10:27:43 -0300

    changing windows and displaying the result

--- scripts.js
+++ scripts.js
@@ -3,19 +3,56 @@ const quantity = document.getElementById("quantity");
 const start = document.getElementById("start");
 const end = document.getElementById("end");
 
+const form = document.querySelector("form");
+const result = document.querySelector(".result");
+
 raffle.addEventListener("click", (event) => {
-  event.preventDefault();
+ event.preventDefault();
 
-  const quantityValue = Number(quantity.value);
-  const startValue = Number(start.value);
+ const quantityValue = Number(quantity.value);
+ const startValue = Number(start.value);
+ const endValue = Number(end.value);
 
-  const endValue = Number(end.value);
+ const drawnNumber =
+ Math.floor(Math.random() * (endValue - startValue + 1)) + startValue;
 
-  const randomNumber = Math.floor(Math.random() * (endValue - startValue + 1)) + startValue;
-  console.log(randomNumber);
+ const title = document.createElement("h4");
+ title.textContent = "Draw result";
+ const subtitle = document.createElement("p");
+ subtitle.textContent = "1st result";
+ const resultContainer = document.createElement("span");
+ resultContainer.textContent = drawnNumber;
+
+ const buttonAgain = document.createElement("button");
+ const img = document.createElement("img");
+ img.setAttribute("src", "./assets/again.svg");
+ buttonAgain.classList.add("again");
+ buttonAgain.textContent = "Draw again";
+ buttonAgain.appendChild(img);
+
+ result.appendChild(title);
+ result.appendChild(subtitle);
+ result.appendChild(resultContainer);
+ result.appendChild(buttonAgain);
+
+ toggleScreen();
+ buttonAgain.addEventListener("click", () => {
+ buttonAgain.classList.add("hidden");
+ result.classList.add("hidden");
+ form.classList.remove("hidden");
+ });
+ clearInput();
 });
 
-function computerSelectionOfNumber() {
+function randomNumber() {}
 
- 
+function toggleScreen() {
+ form.classList.toggle("hidden");
+ result.classList.toggle("hidden");
+}
+
+function clearInput() {
+ quantity.value = "";
+ start.value = "";
+ end.value = "";
 }
